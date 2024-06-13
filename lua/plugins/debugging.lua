0e4113b751b4878bb413fff2ef1ec49697a8cdb0
Carlos Gomez <gohercarlos@gmail.com>
2024-06-13 11:44:57 -0500

    New plugins

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,33 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"leoluz/nvim-dap-go",
+		"rcarriga/nvim-dap-ui",
+    "tpope/vim-fugitive",
+    "folke/trouble.nvim",
+    "nvim-neotest/nvim-nio"
+	},
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    require("dapui").setup()
+    require("dap-go").setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+  end,
+}
